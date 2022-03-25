00eda7b141a423ce70b1d4eec4a54188ad5e1624
humber-jake <humber.jake@gmail.com>
2022-03-25 00:40:45 -0700

    Style main page

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,12 +1,18 @@
 import sizes from './sizes'
+import bg from './bg.svg'
+
+// Background courtesy of svgbackgrounds.com
 
 const styles = {
     root: {
-        backgroundColor: 'darkgray',
+        backgroundColor: '#FFF6DC',
+        backgroundImage: `url(${bg})`,
+        backgroundAttachment: 'scroll',
         height: '100%',
         display: 'flex',
         alignItems: 'flex-start',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        overflow: 'auto'
     },
     container: {
         width: '50%',
@@ -26,10 +32,23 @@ const styles = {
         width: '100%',
         justifyContent: 'space-between',
         color: 'white',
-        alignItems: 'center',
+        alignItems: 'flex-end',
         "& a": {
+            fontSize: '1.5rem',
             textDecoration: 'none',
             color: 'white',
+            marginRight: '1.5rem',
+            marginBottom: '0.2rem',
+            textShadow: '2px 2px #99badd',
+            '&:hover':{
+                textShadow: '0px 0px 10px white'
+            }   
+        },
+        "& h1": {
+            fontSize: '3rem',
+            marginBottom: 0,
+            textShadow: '3px 3px #99badd'
+            
         }
     },
     palettes: {
@@ -44,6 +63,13 @@ const styles = {
         [sizes.andDown("xs")]: {
             gridTemplateColumns: "repeat(1, 100%)",
             gridGap: "1.4rem"
+        },
+        '& a': {
+            borderRadius: '4px',
+            boxShadow: '3px 3px #99badd',
+            '&:hover':{
+                boxShadow: '0px 0px 10px white'
+            }   
         }
     },
     link: {
