733e3984a64472f65f54faa648a84cb0c0c3b238
humber-jake <humber.jake@gmail.com>
2022-03-24 20:34:11 -0700

    style tweaks

--- src/NewPaletteFormNav.js
+++ src/NewPaletteFormNav.js
@@ -6,7 +6,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import {AddCircleOutlineOutlined} from '@material-ui/icons/';
 import Button from '@material-ui/core/Button'
 import { withStyles } from '@material-ui/core';
 import NewPaletteDialogue from './NewPaletteDialogue';
@@ -45,9 +45,9 @@ function NewPaletteFormNav(props) {
                     aria-label="open drawer"
                     onClick={handleDrawerOpen}
                     edge="start"
-                    className={clsx(classes.menuButton, open && classes.hide)}
+                    className={clsx(classes.addCircle, open && classes.hide)}
                 >
-                    <MenuIcon />
+                    <AddCircleOutlineOutlined />
                 </IconButton>
                 <Typography variant="h6" noWrap>
                     Create New Palette
